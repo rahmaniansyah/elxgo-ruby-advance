8ede906b507a2fe2900996e8687baf72ac2f8c8f
rahma_putri13 <rahmaputri13@gmail.com>
2021-02-07 09:58:31 +0700

    [TEST] exercise #transforming_aoi returns false when input is not integer

--- day-1/spec/exercise_spec.rb
+++ day-1/spec/exercise_spec.rb
@@ -7,11 +7,18 @@ require './models/exercise.rb'
 
 RSpec.describe Exercise do
     context '#incrementing_aoi' do
-        it 'returns true when input is integer' do
+        it 'returns false when input not integer' do
             incrementer = Exercise.new
-            result = incrementer.incrementing_aoi(5)
+            result = incrementer.incrementing_aoi('a')
 
-            expect(result).to eq true
+            expect(result).to eq false
         end
+
+        # it 'returns 54 when input is [5,4]' do
+        #     incrementer = Exercise.new
+        #     result = incrementer.incrementing_aoi([5,4])
+
+        #     expect(result).to eq 54
+        # end
     end
 end
