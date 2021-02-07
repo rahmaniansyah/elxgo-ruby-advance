cb79f3fedecf7810a85b1b5881d37cfc4bb2ceb6
rahma_putri13 <rahmaputri13@gmail.com>
2021-02-07 12:02:59 +0700

    [REFACTOR]#incrementing_aoi create convert_to_array method for output

--- day-1/models/exercise.rb
+++ day-1/models/exercise.rb
@@ -3,19 +3,25 @@ class Exercise
         if input.empty?
             return [1]
         else
-            result = Array.new
             sum = 0
             input_size = input.size - 1
             input.each do |number|
                 sum += number * (10**(input_size))
                 input_size -= 1
             end
-            sum += 1
-            while sum > 0
-                result << sum % 10
-                sum /= 10
-            end
-            result.reverse
+            result = convert_to_array(sum += 1)
         end
+    end
+
+    private
+
+    def convert_to_array(input)
+        result = Array.new
+
+        while input > 0
+            result.push(input % 10)
+            input /= 10
+        end
+        result.reverse
     end
 end
