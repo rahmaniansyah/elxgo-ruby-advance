f43ddbe72ff4dcb1185d12b5c24b1dca11a02aa3
rahma_putri13 <rahmaputri13@gmail.com>
2021-02-07 11:44:21 +0700

    COBA ULANG TDD DARI AWAL :)

--- day-1/models/exercise.rb
+++ day-1/models/exercise.rb
@@ -1,25 +1,28 @@
 class Exercise
     def incrementing_aoi(input)
-        if input.empty?
-            return 1
-        else
-            array_size = input.size
-            result = 0
-            input.each do |number|
-                if !number.is_a? Integer
-                    return false
-                else
-                    result += number * (10**(array_size-1))
-                    array_size -= 1
-                end
-            end
-    
-            result + 1
-        end
+        # if input.empty?
+        #     return 1
+        # else
+        #     array_size = input.size - 1
+        #     result = 0
+        #     input.each do |number|
+        #         if !number.is_a? Integer
+        #             return false
+        #         else
+        #             result += number * (10**(array_size))
+        #             array_size -= 1
+        #         end
+        #     end
+        #     result += 1
+        #     fix_res = Array.new
+        #     array_size = input.size - 2
+        #     result.each do |number|
+        #         if number / (10**(array_size))
+        #             fix_res.push(number / (10**(array_size)))
+        #         end
+        #     end
+        # end
 
+        # return fix_res
     end
-
-    # def convert_increment(input)
-        
-    # end
 end
