249ad68802083fc7a719c2e307e3ff707e61e3d7
rahma_putri13 <rahmaputri13@gmail.com>
2021-02-07 20:33:49 +0700

    [TEST] MessageOperation#send_email return Failure when sending email

--- day-2/spec/message_operation_spec.rb
+++ day-2/spec/message_operation_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe MessageOperation do
            allow(MessageOperation).to receive(:send_email).and_return("Email successfully sent")
            expect(described_class.send_email).to eq "Email successfully sent" 
         end
+
+        it "return Failure when sending email" do
+            allow(MessageOperation).to receive(:send_email).and_return("Failure when sending email")
+            expect(described_class.send_email).to eq "Failure when sending email" 
+         end
     end
 end
