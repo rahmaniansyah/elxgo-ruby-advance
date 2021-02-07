1ff9675b39e9fa6118b0ba4424112d0527e980a9
rahma_putri13 <rahmaputri13@gmail.com>
2021-02-07 11:59:26 +0700

    [TEST]#incrementing_aoi returns [1,0,0] when input is [9,9]

--- day-1/spec/exercise_spec.rb
+++ day-1/spec/exercise_spec.rb
@@ -27,5 +27,12 @@ RSpec.describe Exercise do
 
             expect(result).to eq [5,6,2]
         end
+
+        it 'returns [1,0,0] when input is [9,9]' do
+            incrementer = Exercise.new
+            result = incrementer.incrementing_aoi([9,9])
+
+            expect(result).to eq [1,0,0]
+        end
     end
 end
