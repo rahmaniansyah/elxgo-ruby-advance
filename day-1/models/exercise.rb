ac0ee2fbc639e628eebea862b245d069b6d64327
rahma_putri13 <rahmaputri13@gmail.com>
2021-02-07 10:35:11 +0700

    [REFACTORING] returns 55 when input is [5,4]

--- day-1/models/exercise.rb
+++ day-1/models/exercise.rb
@@ -2,20 +2,21 @@ class Exercise
     def incrementing_aoi(input)
         if input.empty?
             return 1
-        end
-
-        array_size = input.size
-        result = 0
-        input.each do |number|
-            if !number.is_a? Integer
-                return false
-            else
-                result += number * (10**(array_size-1))
-                array_size -= 1
+        else
+            array_size = input.size
+            result = 0
+            input.each do |number|
+                if !number.is_a? Integer
+                    return false
+                else
+                    result += number * (10**(array_size-1))
+                    array_size -= 1
+                end
             end
+    
+            result + 1
         end
 
-        result + 1
     end
 
     # def convert_increment(input)
