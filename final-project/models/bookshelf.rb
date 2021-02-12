c8d2cd5356dc76d2f3483462e7653f50aa5db49d
iput13 <rahma.putri@mekari.com>
2021-02-12 21:55:58 +0700

    [REFACTOR] change first test #generate_shelf_index return [010101, 010102] when input 1,1,2

--- final-project/models/bookshelf.rb
+++ final-project/models/bookshelf.rb
@@ -1,23 +1,36 @@
 class Bookshelf
 
-    attr_accessor :indexes
+    attr_accessor :book_ids, :rows, :columns
 
-    def initialize(indexes = nil)
-        @indexes = indexes
+    def initialize(book_ids = nil, rows = nil, columns = nil)
+        @book_ids = book_ids
+        @rows = rows
+        @columns = columns
     end
 
-    def generate_shelf_indexes(params)
+    def generate_shelf_indexes(book_ids, rows, columns)
         shelf_indexes = Array.new
-        for row in 1..params["row"]
-            for column in 1..params["column"]
-                shelf_indexes.push("0#{params["id"]}0#{row}0#{column}")
+            for row in 1..rows
+                for column in 1..columns
+                    shelf_indexes.push("0#{book_ids}0#{row}0#{column}")
+                end
             end
-        end
-        
         shelf_indexes
     end
 
-    def self.build
+    # def self.build
+    #     indexes = generate_shelf_indexes(book_ids, rows, columns)
+    #     shelves = {}
+    #     indexes.inject({}) do |hash, index|
+    #         hash[index] = nil
+    #         shelves = hash
+    #     end
+    #     shelves
         
-    end
+    #     if shelves.size == book_ids*rows*columns
+    #         for book_id in 1..book_ids
+    #             puts "Shelf #{book_id} with #{rows} rows and #{columns} columns is added"
+    #         end
+    #     end
+    # end
 end
--- final-project/spec/bookshelf_spec.rb
+++ final-project/spec/bookshelf_spec.rb
@@ -2,25 +2,31 @@ require './models/bookshelf.rb'
 
 RSpec.describe Bookshelf do
     context '#generate_shelf_index' do
-        it 'return array of string when input bookshelf params id, row, column' do
-            bookshelf_params = {"id"=> 1, "row"=> 1, "column"=> 2}
-            
+        it 'return ["010101", "010102"] when input 1,1,2' do
             bookshelf = Bookshelf.new
-            result = bookshelf.generate_shelf_indexes(bookshelf_params)
+            result = bookshelf.generate_shelf_indexes(1,1,2)
             
             expeted_result = ["010101", "010102"]
             expect(result).to eq expeted_result
         end
     end
 
-    # context '#build' do
+    # context '.build' do
+    #     it 'return Shelf 1 with 1 rows and 2 columns is added when input build_library|1|1|2' do
+    #         bookshelf = Bookshelf.new(book_id, row, column)
+    #         expect(bookshelf.create).to eq "Shelf 1 with 1 rows and 3 columns is added"
+    #     end
+    # end
+
+    # context '.build' do
     #     it 'return Shelf 1 with 1 rows and 2 columns is added when input build_library|1|1|2' do
     #         input_user = "build_library|2|1|3"
-            
     #         shelf_indexes = ["010101", "010102"]
-    #         bookshelf = Bookshelf.new
+    #         bookshelf_params = {"id"=> 1, "row"=> 1, "column"=> 2}
             
-    #         expect(shelf_indexes.bookshelf.build).to eq "Shelf 1 with 1 rows and 3 columns is added"
+    #         bookshelf = Bookshelf.new(shelf_indexes, bookshelf_params)
+            
+    #         expect(bookshelf.build).to eq "Shelf 1 with 1 rows and 3 columns is added"
     #     end
     # end
 end
