1cb0730b2003e04e7905ae49e1e8b3c59aa77e8c
iput13 <rahma.putri@mekari.com>
2021-02-14 13:29:00 +0700

    [REFACTOR] move command in main to class command_parser

--- final-project/main.rb
+++ final-project/main.rb
@@ -1,37 +1,4 @@
-require './models/library.rb'
+require './models/command_parser.rb'
 
-puts "----------------------"
-puts "Welcome in Library"
-puts "----------------------"
-
-begin
-    input = gets.chomp
-    command, *args = input.split("|")
-
-    case command
-    when 'build_library'
-        bookshelves = args[0].to_i
-        rows = args[1].to_i
-        columns = args[2].to_i
-        
-        puts "#{bookshelves} #{rows} #{columns}"
-        
-        library = Library.new(bookshelves,rows,columns)
-        libraries = library.build
-        libraries.each {|library| puts library}
-    when 'put_book'
-        puts 'put_book'
-    when 'take_book_form'
-        puts 'take_book_form'
-    when 'find_book'
-        puts 'find_book'
-    when 'list_books'
-        puts 'list_books'
-    when 'search_books_by_title'
-        puts 'search_books_by_title'
-    when 'search_books_by_author'
-        puts 'search_books_by_author'
-    else
-        puts 'sorry incorrect command'
-    end
-end while command != 'exit'
+command_parser = CommandParser.new
+command_parser.run
